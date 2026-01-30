a43ad492d829c1056d55eb2581adfac227cc4869
Connor Perkkio <cperkkio@mocs.flsouthern.edu>
2026-01-30 14:59:27 -0500

    Made the signup and login views/list

--- cyberlab/accounts/views.py
+++ cyberlab/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+ from django.shortcuts import render
+ from .info import SignUpForm
 
-# Create your views here.
+ def signup_view(request):
+      created = False
+      if request.method == "POST":
+          form = SignUpForm(request.POST)
+          if form.is_valid():
+              form.save()
+              created = True
+              form = SignUpForm()
+      else:
+          form = SignUpForm()
+      return render(request, "accounts/signup.html", {"form": form, "created": created})
+
+  def login_view(request):
+      return render(request, "accounts/login.html")
