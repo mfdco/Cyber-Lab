dc58e0cd9653156e78de27a99c752247c95cb2a9
Angelo Pira <angelojamespira@gmail.com>
2026-02-17 22:07:00 -0500

    Redirect signup to dashboard and removed redundent signupsuccess template

--- cyberlab/accounts/views.py
+++ cyberlab/accounts/views.py
@@ -9,7 +9,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
     else:
         form = SignUpForm()
 
