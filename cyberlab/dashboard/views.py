8b319afd2f551cdf7098c4ca38087e87ca6d4961
Angelo Pira <angelojamespira@gmail.com>
2026-02-26 14:40:09 -0500

    Add submission history page

--- cyberlab/dashboard/views.py
+++ cyberlab/dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from challenges.models import Submission
 
 
 def dashboard_view(request):
@@ -13,3 +14,11 @@ def profile_view(request):
         return redirect('login')
 
     return render(request, 'dashboard/profile.html', {'user': request.user})
+
+
+def history_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    submissions = Submission.objects.filter(user=request.user).order_by('-submitted_at')
+    return render(request, 'dashboard/history.html', {'user': request.user, 'submissions': submissions})
