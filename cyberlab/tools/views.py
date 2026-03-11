1181fadc41c162e6ca992ecb664fb74b91621540
Connor <capperkkio@gmail.com>
2026-03-11 14:37:37 -0400

    added the base64 views and make a test case

--- cyberlab/tools/views.py
+++ cyberlab/tools/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .utils import decode_base64, encode_base64
+
+
+# This view takes user input, then either encodes or decodes it with Base64.
+def base64_tool(request):
+    result = ""
+
+    if request.method == "POST":
+        text = request.POST.get("text", "")
+        action = request.POST.get("action", "")
+
+        if action == "encode":
+            result = encode_base64(text)
+        elif action == "decode":
+            result = decode_base64(text)
+
+    return render(request, "tools/base64.html", {"result": result})
