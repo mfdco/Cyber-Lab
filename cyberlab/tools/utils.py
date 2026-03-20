73ca880995d9e2ff9447b364cd0eaae2d3037fcc
Connor <capperkkio@gmail.com>
2026-03-20 16:17:52 -0400

    added binary to the tools

--- cyberlab/tools/utils.py
+++ cyberlab/tools/utils.py
@@ -11,3 +11,18 @@ def decode_base64(text):
     encoded_bytes = text.encode("utf-8")
     decoded_bytes = base64.b64decode(encoded_bytes)
     return decoded_bytes.decode("utf-8")
+
+
+def encode_binary(text):
+    binary_text = []
+    for letter in text:
+        binary_text.append(format(ord(letter), "08b"))
+    return " ".join(binary_text)
+
+
+def decode_binary(text):
+    binary_text = text.split(" ")
+    decoded_text = []
+    for binary_letter in binary_text:
+        decoded_text.append(chr(int(binary_letter, 2)))
+    return "".join(decoded_text)
